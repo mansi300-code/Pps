4472a922aab8de4176a3699a6398c604a6dbb9ce
mansi300-code <parmarmansi62523@gmail.com>
2026-02-14 10:45:02 +0530

    Change values of variables a and b
    Updated the values of variables 'a' and 'b' in P1.c.

---
+++ P1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+  printf ("\n arithmetic");
+int a=20;
+int b=30;
+printf ("enter value of a and b");
+printf (" addition=%d",a+b);
+printf ("sub=%d",a-b);
+printf ("multi=%d",a*b);
+printf ("div=%d",a/b);
+printf ( "module=%d",a%b);
+return 0;
+}
